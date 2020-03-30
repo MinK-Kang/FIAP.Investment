048d12196489fad9c4e5e70f7e8714827cff0c73
Min Koo Kang <minkoo0619@gmail.com>
2020-03-29 21:49:31 -0300

    Limpando array não utilizado.

--- FIAP.API/Controllers/InvestmentController.cs
+++ FIAP.API/Controllers/InvestmentController.cs
@@ -11,11 +11,6 @@ namespace FIAP.Fase5.Controllers
     [Route("[controller]")]
     public class InvestmentController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<InvestmentController> _logger;
 
         public InvestmentController(ILogger<InvestmentController> logger)
